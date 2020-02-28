6a7a8c989d374ff35f02000df4f1adc9b42e1e49
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-28 22:32:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,14 @@
 class Application
   @@items = []
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last 
+      item_name = req.path.split("/items/").last
       item = @@items.find do {|item| item.name == item_name}
-      
+
       if item != nil
         resp.write item.price
       else
